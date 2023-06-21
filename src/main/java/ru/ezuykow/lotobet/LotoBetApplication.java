ae7f05e62e4e115c81b618029e994dd74c2a48f5
ezuykow <zuy1996@gmail.com>
2023-06-21 08:22:16 +0300

    Initial commit

---
+++ src/main/java/ru/ezuykow/lotobet/LotoBetApplication.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.lotobet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotoBetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotoBetApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/ezuykow/lotobet/configs/AppConfig.java
@@ -0,0 +1,7 @@
+package ru.ezuykow.lotobet.configs;
+
+/**
+ * @author ezuykow
+ */
+public class AppConfig {
+}
